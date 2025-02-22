9cf6034b91fbc09b93a74e49e1db57ca0f6dc559
Yuliia_Sheremeta <sheremeyulya1996@gmail.com>
2025-02-22 13:32:22 +0100

    add styles to table

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -2,20 +2,20 @@ import css from './TransactionHistory.module.css';
 
 export default function TransactionHistory({ items }) {
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Type</th>
-                    <th>Amount</th>
-                    <th>Currency</th>
+        <table className={css.table}>
+            <thead className={css.thead}>
+                <tr className={css.theadText}>
+                    <th className={css.tr}>Type</th>
+                    <th className={css.tr}>Amount</th>
+                    <th className={css.tr}>Currency</th>
                 </tr>
             </thead>
             <tbody>
-                {items.map(item => (
-                <tr key={item.id}>
-                    <td>{item.type}</td>
-                    <td>{item.amount}</td>
-                    <td>{item.currency}</td>
+                {items.map((item, index) => (
+                <tr key={item.id} className={index % 2 === 0 ? css.backgroundFirst : css.backgroundSecond}>
+                    <td className={css.tr}>{item.type}</td>
+                    <td className={css.tr}>{item.amount}</td>
+                    <td className={css.tr}>{item.currency}</td>
                 </tr>
             ))}
             </tbody>
